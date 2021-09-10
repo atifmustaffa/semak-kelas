2abceeccf11686a38199b8f95f7b7516439640f4
Atif Mustaffa <atifmustaffa@gmail.com>
2021-09-11 01:37:31 +0800

    feat: Add update electron app

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow, ipcMain, Menu, shell } = require('electron')
 const path = require('path')
+const updateApp = require('update-electron-app')
 const pkgJSON = require('./package.json')
 require('@electron/remote/main').initialize()
 const Store = require('electron-store')
@@ -110,6 +111,12 @@ app.whenReady().then(() => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+
+  updateApp({
+    // repo: pkgJSON.repository, // defaults to package.json
+    updateInterval: '1 hour',
+    notifyUser: true,
   })
 })
 
